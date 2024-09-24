66123180a5ab43f9fb3395f6980dfc165c2e4032
Edmondo-diRonza <eddironz@hotmail.com>
2024-09-24 17:27:17 +0200

    abbiamo finito tutto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./**/*.liquid"],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      main: "#3A3F4C",
+      black: "#000000",
+    },
   },
   plugins: [],
 };
